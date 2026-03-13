c0daba2520f2adeb25d966caefbda4ed081f83b7
iamchioma <chiomalinaanaso@gmail.com>
2026-03-13 12:13:42 +0100

    feat(testimonials): create Testimonial component with reusable Title and mount on Home page

---
+++ src/components/Testimonial.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Title from "./Title";
+
+const Testimonial = () => {
+  return (
+    <div className="flex flex-col items-center px-6 md:px-16 lg:px-24 bg-slate-50 pt-20 pb-30">
+      <Title
+        title="What our Guests say"
+        subTitle="Discover why discerning travelers consistently choose SilverLeaf for their exclusive and luxurious accomodations around the world."
+      />
+    </div>
+  );
+};
+
+export default Testimonial;
