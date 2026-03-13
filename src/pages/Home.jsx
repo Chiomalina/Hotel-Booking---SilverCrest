ea11dd4cdf9895d34e7e0183742d4df39ce1d6e5
iamchioma <chiomalinaanaso@gmail.com>
2026-03-13 11:04:50 +0100

    feat(home): implement reusable Title component and update FeaturedDestination layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Hero from "../components/Hero";
 import FeaturedDestination from "../components/FeaturedDestination";
+import Title from "../components/Title";
 
 const Home = () => {
   return (
