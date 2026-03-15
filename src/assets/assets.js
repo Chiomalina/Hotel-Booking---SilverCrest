08b0bbcdb0b289e00574624b9db1010ad15f6b37
iamchioma <chiomalinaanaso@gmail.com>
2026-03-15 06:11:08 +0100

    feat(room-details): finalize RoomDetails page UI and functionality
    - implement dynamic room data loading using useParams
    - add interactive room image gallery with selectable thumbnails
    - display amenities with facility icons
    - implement booking form with check-in/out dates and guest validation
    - add room specifications and pricing section
    - include host information and contact section

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -31,6 +31,7 @@ import regImage from "./regImage.png";
 import exclusiveOfferCardImg1 from "./exclusiveOfferCardImg1.png";
 import exclusiveOfferCardImg2 from "./exclusiveOfferCardImg2.png";
 import exclusiveOfferCardImg3 from "./exclusiveOfferCardImg3.png";
+import hotelHostGoldPremium from "./hotelHostGoldPremium.png";
 import addIcon from "./addIcon.svg";
 import dashboardIcon from "./dashboardIcon.svg";
 import listIcon from "./listIcon.svg";
@@ -178,8 +179,7 @@ export const userDummyData = {
   _id: "user_2unqyL4diJFP1E3pIBnasc7w8hP",
   username: "Great Stack",
   email: "user.greatstack@gmail.com",
-  image:
-    "https://img.clerk.com/eyJ0eXBlIjoicHJveHkiLCJzcmMiOiJodHRwczovL2ltYWdlcy5jbGVyay5kZXYvdXBsb2FkZWQvaW1nXzJ2N2c5YVpSSEFVYVUxbmVYZ2JkSVVuWnFzWSJ9",
+  image: hotelHostGoldPremium,
   role: "hotelOwner",
   createdAt: "2025-03-25T09:29:16.367Z",
   updatedAt: "2025-04-10T06:34:48.719Z",
