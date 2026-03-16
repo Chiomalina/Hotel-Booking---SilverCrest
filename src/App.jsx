1e384d6a601c397e33a0aec2512881280f687246
iamchioma <chiomalinaanaso@gmail.com>
2026-03-16 04:36:23 +0100

    feat(owner-dashboard): build dashboard analytics view with booking stats and recent activity table

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,9 @@ import RoomDetails from "./pages/RoomDetails";
 import MyBookings from "./pages/MyBookings";
 import HotelReg from "./components/HotelReg";
 import Layout from "./pages/HotelOwner/Layout";
+import Dashboard from "./pages/HotelOwner/Dashboard";
+import ListRoom from "./pages/HotelOwner/ListRoom";
+import AddRoom from "./pages/HotelOwner/AddRoom";
 
 const App = () => {
   //Hide Navbar if pathname is owner
@@ -22,7 +25,11 @@ const App = () => {
           <Route path="/rooms" element={<AllRooms />} />
           <Route path="/rooms/:id" element={<RoomDetails />} />
           <Route path="/my-bookings" element={<MyBookings />} />
-          <Route path="/owner" element={<Layout />}></Route>
+          <Route path="/owner" element={<Layout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="add-room" element={<AddRoom />} />
+            <Route path="list-room" element={<ListRoom />} />
+          </Route>
         </Routes>
       </div>
       <Footer />
